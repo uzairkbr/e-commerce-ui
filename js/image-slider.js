d1f9b9ba8cb1df7e8b8c1e77a2e81865ab62b902
uzairahmad8 <uzairdev8@gmail.com>
2024-09-01 00:07:13 +0500

    Fixed: Product Page

--- js/image-slider.js
+++ js/image-slider.js
@@ -1,3 +1,60 @@
+// function imageSlider(sliderClass, slideClass, dotsClass, prevButtonId, nextButtonId) {
+//   const slider = document.querySelector(sliderClass);
+//   const slides = document.querySelectorAll(slideClass);
+//   const dotsContainer = document.querySelector(dotsClass);
+//   const prevButton = document.getElementById(prevButtonId);
+//   const nextButton = document.getElementById(nextButtonId);
+
+//   let currentSlide = 0;
+//   const totalSlides = slides.length;
+
+//   for (let i = 0; i < totalSlides; i++) {
+//     const img = document.createElement("img");
+//     img.classList.add("slider-dot-image");
+//     img.src = slides[i].src;
+//     img.alt = `Slide ${i + 1}`;
+//     dotsContainer.appendChild(img);
+//   }
+
+//   const dots = document.querySelectorAll(`${dotsClass} .slider-dot-image`);
+
+//   const resetSlides = () => {
+//     for (let i = 0; i < totalSlides; i++) {
+//       slides[i].style.display = "none";
+//       dots[i].style.opacity = "0.5";
+//     }
+//   };
+
+//   const renderSlide = () => {
+//     resetSlides();
+//     slides[currentSlide].style.display = "block";
+//     dots[currentSlide].style.opacity = "1";
+//   };
+
+//   dots.forEach((dot, index) => {
+//     dot.addEventListener("click", function() {
+//       currentSlide = index;
+//       renderSlide();
+//     });
+//   });
+
+//   prevButton.addEventListener("click", function () {
+//     currentSlide = currentSlide > 0 ? --currentSlide : totalSlides - 1;
+//     renderSlide();
+//   });
+
+//   nextButton.addEventListener("click", function () {
+//     currentSlide = currentSlide < totalSlides - 1 ? ++currentSlide : 0;
+//     renderSlide();
+//   });
+
+//   renderSlide();
+// }
+
+// imageSlider(".product-slider", ".product-slide", ".product-slider-dots", "product-left-icon", "product-right-icon");
+
+
+
 function imageSlider(sliderClass, slideClass, dotsClass, prevButtonId, nextButtonId) {
   const slider = document.querySelector(sliderClass);
   const slides = document.querySelectorAll(slideClass);
@@ -18,37 +75,35 @@ function imageSlider(sliderClass, slideClass, dotsClass, prevButtonId, nextButto
 
   const dots = document.querySelectorAll(`${dotsClass} .slider-dot-image`);
 
-  const resetSlides = () => {
-    for (let i = 0; i < totalSlides; i++) {
-      slides[i].style.display = "none";
-      dots[i].style.opacity = "0.5";
-    }
-  };
+  const renderSlide = (slide) => {
+    slides.forEach((s, index) => {
+      s.style.transform = `translateX(${100 * (index - slide)}%)`;
+      s.style.transition = "transform 0.5s ease";
+    });
 
-  const renderSlide = () => {
-    resetSlides();
-    slides[currentSlide].style.display = "block";
-    dots[currentSlide].style.opacity = "1";
+    dots.forEach((dot, index) => {
+      dot.style.opacity = index === slide ? "1" : "0.5";
+    });
   };
 
   dots.forEach((dot, index) => {
     dot.addEventListener("click", function() {
       currentSlide = index;
-      renderSlide();
+      renderSlide(currentSlide);
     });
   });
 
   prevButton.addEventListener("click", function () {
     currentSlide = currentSlide > 0 ? --currentSlide : totalSlides - 1;
-    renderSlide();
+    renderSlide(currentSlide);
   });
 
   nextButton.addEventListener("click", function () {
     currentSlide = currentSlide < totalSlides - 1 ? ++currentSlide : 0;
-    renderSlide();
+    renderSlide(currentSlide);
   });
 
-  renderSlide();
+  renderSlide(currentSlide);
 }
 
 imageSlider(".product-slider", ".product-slide", ".product-slider-dots", "product-left-icon", "product-right-icon");
